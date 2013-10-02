e3ac562c27d676cdfdf7190cc8e99de9024d6c13
Eric Hoppe <dash55@gmail.com>
2013-10-01 20:16:50 -0400

    First commit
    Finished die cast script (die_cast.py). I may revisit it down the
    road to do it with regex rather than list comprehension.

---
+++ die_cast.py
@@ -0,0 +1,23 @@
+import random
+
+def cast(stcast):
+    stcast = stcast.replace(" ", "")
+    for letter in stcast:
+        lscast = stcast.split("+")
+    for i in lscast:
+        if i == "+":
+            del lscast[i]
+    total = 0
+    for die in lscast:
+        roll = 0
+        lsdie = die.split("d")
+        if len(lsdie) == 2:
+            if lsdie[0] == "":
+                lsdie[0] = 1
+            while lsdie[0]:
+                roll += random.randrange(1, int(lsdie[1]))
+                lsdie[0] = int(lsdie[0]) - 1
+        else:
+            total += int(die)
+        total += roll
+    return total
