0e2dde44d0f7b38feb9501c89f7b4b9b15c53b1c
Eric Hoppe <hoppeeric@gmail.com>
2013-12-02 20:19:44 -0500

    Expressions via regular expressions
    That was less pleseant than I thought it would be. re.match
    apparently only returns groups if you use groups, so you can't
    group to condense greediness or optionality.
    
    I also stuffed in subtraction expressions to the functions, since
    those actually exist and are prevalent at low levels.

--- combat.py
+++ combat.py
@@ -2,6 +2,6 @@ import die_cast as dc
 
 def initiative(character):
     '''Expected input: Object with initiative instance var.'''
-    roll = dc.die()
+    roll = dc.roll(1, 20)
     roll += character.initiative
     return roll
--- die_cast.py
+++ die_cast.py
@@ -1,31 +1,21 @@
 import random
+import re
+import itertools
 
-def cast(stcast):
-    '''Expected input: String with valid die roll expression (?x[d]y+?z)'''
-    stcast = stcast.replace(" ", "")
-    for letter in stcast:
-        lscast = stcast.split("+")
-    for i in lscast:
-        if i == "+":
-            del lscast[i]
-    total = 0
-    for die in lscast:
-        roll = 0
-        lsdie = die.split("d")
-        if len(lsdie) == 2:
-            if lsdie[0] == "":
-                lsdie[0] = 1
-            while lsdie[0]:
-                roll += random.randrange(1, int(lsdie[1]) + 1)
-                lsdie[0] = int(lsdie[0]) - 1
+def roll_expression(expression):
+    result = 0
+    expression = expression.replace(" ", "")
+    pieces = re.findall('\+?-?\d*d?\d+', expression)
+    for piece in itertools.chain(pieces):
+        die = False
+        if re.match('.*d', piece):
+            die = True
+            n, x = re.findall('\d+', piece)
+            n, x = int(n), int(x)
+        if re.match('-', piece):
+            if die: result -= roll(n, x)
+            else: result += int(piece)
         else:
-            total += int(die)
-        total += roll
-    return total
-
-def die(y = 20, x = 1):
-    roll = 0
-    while x:
-        roll += random.randrange(1, y + 1)
-        x += -1
-    return roll
+            if die: result += roll(n, x)
+            else: result += int(piece)
+    return result
