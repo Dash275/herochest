e8d5bcb7b9d9c9e23cca01ee973e783a4b5911c6
Eric Hoppe <hoppeeric@gmail.com>
2013-12-13 20:10:24 -0500

    Basic statistics

--- hero.py
+++ hero.py
@@ -1,7 +1,27 @@
+import random
+
 class Hero:
-    __init__(self):
+    def __init__(self):
         name = ""
         self.initiative = 0
+        self.strength = random.randrange(3, 19)
+        self.intelligence = random.randrange(3, 19)
+        self.wisdom = random.randrange(3, 19)
+        self.constitution = random.randrange(3, 19)
+        self.dexterity = random.randrange(3, 19)
+        self.charisma = random.randrange(3, 19)
+        self.update_bonuses()
+
+    def __str__(self):
+        return name
+
+    def update_bonuses(self):
+        self.str_bonus = -5 + (self.strength // 2)
+        self.int_bonus = -5 + (self.intelligence // 2)
+        self.wis_bonus = -5 + (self.wisdom // 2)
+        self.con_bonus = -5 + (self.constitution // 2)
+        self.dex_bonus = -5 + (self.dexterity // 2)
+        self.cha_bonus = -5 + (self.charisma // 2)
 
     def name(n):
         '''If an argument, name is argument, else return name.'''
